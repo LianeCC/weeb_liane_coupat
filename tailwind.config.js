121b6994d51232f883b884ec46d1a0051c05d6bb
lianecc <liane.coupat@gmail.com>
2025-07-08 00:33:49 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/* eslint-env node */
+/* global module */
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
